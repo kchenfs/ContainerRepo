462e5094c6e8bb697ce0e3bd0c61d6cf2df6866d
kchenfs <kendxd@gmail.com>
2023-10-21 10:32:29 -0400

    added tls/ssl

--- main.tf
+++ main.tf
@@ -1,7 +1,46 @@
-resource "aws_route53_zone" "main" {
+resource "aws_route53_zone" "myzone" {
   name    = "kchenfs.com"
-  comment = "Managed by Terraform"
+  comment = "Managed by Terraform in main.tf"
 }
+
+resource "aws_acm_certificate" "alb_cert" {
+  domain_name       = "web.kchenfs.com"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "test"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "alb_dns_record" {
+  zone_id = aws_route53_zone.myzone.zone_id
+  name    = "web.kchenfs.com"
+  type    = "A"
+  alias {
+    name                   = aws_lb.container_alb.dns_name
+    zone_id                = aws_lb.container_alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "cert_record" {
+  allow_overwrite = true
+  name =  tolist(aws_acm_certificate.alb_cert.domain_validation_options)[0].resource_record_name
+  records = [tolist(aws_acm_certificate.alb_cert.domain_validation_options)[0].resource_record_value]
+  type = tolist(aws_acm_certificate.alb_cert.domain_validation_options)[0].resource_record_type
+  zone_id = aws_route53_zone.myzone.zone_id
+  ttl = 60
+}
+
+resource "aws_acm_certificate_validation" "alb_cert_validation" {
+  certificate_arn = aws_acm_certificate.alb_cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_record.fqdn]
+}
+
 
 
 resource "aws_lb" "container_alb" {
@@ -17,24 +56,44 @@ resource "aws_lb" "container_alb" {
 }
 
 
-resource "aws_lb_listener" "front_end_listener" {
-  load_balancer_arn = aws_lb.container_alb.arn # Replace with your ALB ARN
-  port              = 80
+resource "aws_lb_listener" "front_end_redirect_to_https" {
+  load_balancer_arn = aws_lb.container_alb.arn
+  port              = "80"
   protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end_target_group.arn
 
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
-resource "aws_lb_target_group" "front_end_target_group" {
+resource "aws_lb_listener" "front_end_https" {
+  load_balancer_arn = aws_lb.container_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.alb_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
+
+
+resource "aws_lb_target_group" "front_end" {
   name        = "fe-target-groups"
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.personal_website_vpc.id
 }
+
 
 # Existing security group resource
 resource "aws_security_group" "alb_sg" {
@@ -43,16 +102,16 @@ resource "aws_security_group" "alb_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_lb" {
   security_group_id = aws_security_group.alb_sg.id
-  from_port   = 0
-  to_port     = 65535
-  ip_protocol    = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = 0
+  to_port           = 65535
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_healthcheck" {
   security_group_id = aws_security_group.alb_sg.id
 
-  ip_protocol        = -1
+  ip_protocol                  = -1
   referenced_security_group_id = aws_security_group.security_group_personal_website.id
 }
 
@@ -134,7 +193,7 @@ resource "aws_ecs_service" "kchenfs_service" {
     assign_public_ip = true
   }
   load_balancer {
-    target_group_arn = aws_lb_target_group.front_end_target_group.arn
+    target_group_arn = aws_lb_target_group.front_end.arn
     container_name   = "web"
     container_port   = 80
   }
