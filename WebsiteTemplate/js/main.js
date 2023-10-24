45877a1e9053c6416b7d934608f310c7bbea816e
kchenfs <kendxd@gmail.com>
2023-10-23 23:26:50 -0400

    adding the location

--- WebsiteTemplate/js/main.js
+++ WebsiteTemplate/js/main.js
@@ -309,7 +309,7 @@
                 zoom: 16 // Google Map ZOOM. You can change this value
             });
             $("#map").addMarker({
-                address: "147 Markham Street, Toronto, Ontario, Canada", // Your Address. Change it
+                address: "Toronto, Ontario, Canada", // Your Address. Change it
             });
         });
     
