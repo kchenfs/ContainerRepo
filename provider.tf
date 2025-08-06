e8b13c182e728358e611f2ab09fa83c0798d3d5e
Ken Chen <Ken.Chen@cantire.com>
2025-08-06 17:50:29 -0400

    consolidated terraform

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.7.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "var.region"
+  profile = "default"
+  # Configuration options
+}
